e21ad02d129e66d594db3b566028047bf1d576e7
WL777777 <wallace.lins19@gmail.com>
2023-02-27 22:29:21 -0300

    Novas Aulas

---
+++ React/exercicio01.js
@@ -0,0 +1,16 @@
+// uma função que receba 2 ou mais parametros e realize uma operação matrmatica qualquer
+
+soma = (a,b) => {
+   return (a+b)
+}
+
+console.log(soma(3,5))
+
+
+const pessoa = {
+    nome:"Wallace",
+    sobrenome: "Lins",
+    idade: 17
+}
+
+console.log("o nome é: ", pessoa.nome, " ,ele tem o sobrenome: ", pessoa.sobrenome, "e ele tem: ", pessoa.idade, "anos." )
---
+++ React/funcoes.js
@@ -0,0 +1,40 @@
+function mensagem (){       
+     let msg = "curinthia"
+     return msg
+     
+}
+    console.log(mensagem()) 
+
+
+variavel = () => {
+    return "oiwjfciahnfha"
+}
+
+console.log(variavel())
+
+
+// algo1 = () => {
+//     mydisplayer("AAAAAAAAAA")
+// }
+
+// algo2 = () =>{
+//     mydisplayer("BBBBBBBBBBBBB")
+// }
+
+// algo1();
+// algo2();
+
+// console.log(algo1())
+
+const nome  = () =>{
+    return("Wallace")
+}
+
+console.log(nome())
+
+lindaMensagem = (msg) =>{
+    return console.log(msg)
+}
+
+lindaMensagem("Java é um lixo")
+lindaMensagem(3)
